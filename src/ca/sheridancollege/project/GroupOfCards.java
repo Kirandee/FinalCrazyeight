305350a576428f754d83ab3a4d3c7cf7c2e1dd7f
Kirandee <59750207+Kirandee@users.noreply.github.com>
2020-04-12 13:05:23 -0400

    Update GroupOfCards.java

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -14,6 +14,7 @@ import java.util.Collections;
  * HINT, you might want to subclass this more than once.
  * The group of cards has a maximum size attribute which is flexible for reuse.
  * @author Sivagama
+ modified by thinkers
  */
 public class GroupOfCards 
 {
